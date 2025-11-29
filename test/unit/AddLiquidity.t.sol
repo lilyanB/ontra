7d31d720c2378248099f6b16d1fb232d3c2352e7
lilyanb <lilyan.bastien@gmail.com>
2025-11-29 21:00:16 +0100

    test: increase test

--- test/unit/AddLiquidity.t.sol
+++ test/unit/AddLiquidity.t.sol
@@ -4,9 +4,7 @@ pragma solidity ^0.8.0;
 import {Test} from "forge-std/Test.sol";
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 import {Currency, CurrencyLibrary} from "v4-core/types/Currency.sol";
-import {ModifyLiquidityParams} from "v4-core/types/PoolOperation.sol";
 import {StateLibrary} from "v4-core/libraries/StateLibrary.sol";
-import {BalanceDelta, toBalanceDelta} from "v4-core/types/BalanceDelta.sol";
 import {Position} from "v4-core/libraries/Position.sol";
 
 import {OntraHookFixture} from "./utils/Fixtures.sol";
@@ -21,6 +19,8 @@ contract TestAddLiquidity is OntraHookFixture {
      */
     function test_addLiquidity_withinCurrentTickRange() public {
         (, int24 currentTick,,) = manager.getSlot0(key.toId());
+        uint256 amountToken0 = 50 ether;
+        uint256 amountToken1 = 50 ether;
 
         // Add liquidity around current tick
         int24 tickLower = currentTick - 60;
@@ -32,21 +32,23 @@ contract TestAddLiquidity is OntraHookFixture {
         uint256 aaveToken0Before = aavePool.getUserBalance(address(hook), address(token0));
         uint256 aaveToken1Before = aavePool.getUserBalance(address(hook), address(token1));
 
-        uint128 liquidity = hook.addLiquidity(key, tickLower, tickUpper, 50 ether, 50 ether);
-
-        // Verify balances after
-        uint256 token0BalanceAfter = token0.balanceOf(address(hook));
-        uint256 token1BalanceAfter = token1.balanceOf(address(hook));
-        uint256 aaveToken0After = aavePool.getUserBalance(address(hook), address(token0));
-        uint256 aaveToken1After = aavePool.getUserBalance(address(hook), address(token1));
+        uint128 liquidity = hook.addLiquidity(key, tickLower, tickUpper, amountToken0, amountToken1);
 
         // Hook should not have received any tokens directly
-        assertEq(token0BalanceAfter, token0BalanceBefore, "Hook token0 balance should not change");
-        assertEq(token1BalanceAfter, token1BalanceBefore, "Hook token1 balance should not change");
+        assertEq(token0.balanceOf(address(hook)), token0BalanceBefore, "Hook token0 balance should not change");
+        assertEq(token1.balanceOf(address(hook)), token1BalanceBefore, "Hook token1 balance should not change");
 
         // Liquidity should stay in the pool manager (not deposited to Aave)
-        assertEq(aaveToken0After, aaveToken0Before, "No token0 should be deposited to Aave");
-        assertEq(aaveToken1After, aaveToken1Before, "No token1 should be deposited to Aave");
+        assertEq(
+            aavePool.getUserBalance(address(hook), address(token0)),
+            aaveToken0Before,
+            "No token0 should be deposited to Aave"
+        );
+        assertEq(
+            aavePool.getUserBalance(address(hook), address(token1)),
+            aaveToken1Before,
+            "No token1 should be deposited to Aave"
+        );
 
         // Verify liquidity was added
         assertGt(liquidity, 0, "Liquidity should be positive");
@@ -77,6 +79,8 @@ contract TestAddLiquidity is OntraHookFixture {
      */
     function test_addLiquidity_outsideCurrentTickRange_above() public {
         (, int24 currentTick,,) = manager.getSlot0(key.toId());
+        uint256 amountToken0 = 0;
+        uint256 amountToken1 = 100 ether;
 
         // Add liquidity above current tick (only token1 should be deposited)
         int24 tickLower = currentTick + 120;
@@ -89,25 +93,28 @@ contract TestAddLiquidity is OntraHookFixture {
         uint256 aaveToken1Before = aavePool.getUserBalance(address(hook), address(token1));
         uint256 userToken1Before = token1.balanceOf(address(this));
 
-        uint128 liquidity = hook.addLiquidity(key, tickLower, tickUpper, 0, 100 ether);
-
-        // Verify balances after
-        uint256 token0BalanceAfter = token0.balanceOf(address(hook));
-        uint256 token1BalanceAfter = token1.balanceOf(address(hook));
-        uint256 aaveToken0After = aavePool.getUserBalance(address(hook), address(token0));
-        uint256 aaveToken1After = aavePool.getUserBalance(address(hook), address(token1));
-        uint256 userToken1After = token1.balanceOf(address(this));
+        uint128 liquidity = hook.addLiquidity(key, tickLower, tickUpper, amountToken0, amountToken1);
 
         // Hook should not hold tokens directly
-        assertEq(token0BalanceAfter, token0BalanceBefore, "Hook token0 balance should not change");
-        assertEq(token1BalanceAfter, token1BalanceBefore, "Hook token1 balance should not change");
+        assertEq(token0.balanceOf(address(hook)), token0BalanceBefore, "Hook token0 balance should not change");
+        assertEq(token1.balanceOf(address(hook)), token1BalanceBefore, "Hook token1 balance should not change");
 
         // Token1 should be deposited to Aave (token0 should not)
-        assertEq(aaveToken0After, aaveToken0Before, "No token0 should be deposited to Aave");
-        assertTrue(aaveToken1After > aaveToken1Before, "Token1 should be deposited to Aave");
+        assertEq(
+            aavePool.getUserBalance(address(hook), address(token0)),
+            aaveToken0Before,
+            "No token0 should be deposited to Aave"
+        );
+        assertEq(
+            aavePool.getUserBalance(address(hook), address(token1)),
+            aaveToken1Before + amountToken1,
+            "100 ether token1 should be deposited to Aave"
+        );
 
         // Verify user paid token1
-        assertTrue(userToken1After < userToken1Before, "User should have paid token1");
+        assertEq(
+            userToken1Before - token1.balanceOf(address(this)), amountToken1, "User should have paid 100 ether token1"
+        );
         assertEq(liquidity, 0, "Liquidity should be 0 for Aave deposits");
     }
 
@@ -117,6 +124,8 @@ contract TestAddLiquidity is OntraHookFixture {
      */
     function test_addLiquidity_outsideCurrentTickRange_below() public {
         (, int24 currentTick,,) = manager.getSlot0(key.toId());
+        uint256 amountToken0 = 100 ether;
+        uint256 amountToken1 = 0;
 
         // Add liquidity below current tick (only token0 should be deposited)
         int24 tickLower = currentTick - 240;
@@ -129,25 +138,28 @@ contract TestAddLiquidity is OntraHookFixture {
         uint256 aaveToken1Before = aavePool.getUserBalance(address(hook), address(token1));
         uint256 userToken0Before = token0.balanceOf(address(this));
 
-        uint128 liquidity = hook.addLiquidity(key, tickLower, tickUpper, 100 ether, 0);
-
-        // Verify balances after
-        uint256 token0BalanceAfter = token0.balanceOf(address(hook));
-        uint256 token1BalanceAfter = token1.balanceOf(address(hook));
-        uint256 aaveToken0After = aavePool.getUserBalance(address(hook), address(token0));
-        uint256 aaveToken1After = aavePool.getUserBalance(address(hook), address(token1));
-        uint256 userToken0After = token0.balanceOf(address(this));
+        uint128 liquidity = hook.addLiquidity(key, tickLower, tickUpper, amountToken0, amountToken1);
 
         // Hook should not hold tokens directly
-        assertEq(token0BalanceAfter, token0BalanceBefore, "Hook token0 balance should not change");
-        assertEq(token1BalanceAfter, token1BalanceBefore, "Hook token1 balance should not change");
+        assertEq(token0.balanceOf(address(hook)), token0BalanceBefore, "Hook token0 balance should not change");
+        assertEq(token1.balanceOf(address(hook)), token1BalanceBefore, "Hook token1 balance should not change");
 
         // Token0 should be deposited to Aave (token1 should not)
-        assertTrue(aaveToken0After > aaveToken0Before, "Token0 should be deposited to Aave");
-        assertEq(aaveToken1After, aaveToken1Before, "No token1 should be deposited to Aave");
+        assertEq(
+            aavePool.getUserBalance(address(hook), address(token0)),
+            amountToken0 + aaveToken0Before,
+            "100 ether token0 should be deposited to Aave"
+        );
+        assertEq(
+            aavePool.getUserBalance(address(hook), address(token1)),
+            aaveToken1Before,
+            "No token1 should be deposited to Aave"
+        );
 
         // Verify user paid token0
-        assertTrue(userToken0After < userToken0Before, "User should have paid token0");
+        assertEq(
+            userToken0Before - token0.balanceOf(address(this)), amountToken0, "User should have paid 100 ether token0"
+        );
         assertEq(liquidity, 0, "Liquidity should be 0 for Aave deposits");
     }
 
@@ -157,6 +169,8 @@ contract TestAddLiquidity is OntraHookFixture {
      */
     function test_liquidityMovesToAave_afterPriceCross() public {
         (, int24 currentTick,,) = manager.getSlot0(key.toId());
+        uint256 amountToken0First = 50 ether;
+        uint256 amountToken1First = 50 ether;
 
         // First add liquidity within range
         int24 tickLower = currentTick - 60;
@@ -165,7 +179,7 @@ contract TestAddLiquidity is OntraHookFixture {
         uint256 aaveToken0Before = aavePool.getUserBalance(address(hook), address(token0));
         uint256 aaveToken1Before = aavePool.getUserBalance(address(hook), address(token1));
 
-        uint128 liquidity1 = hook.addLiquidity(key, tickLower, tickUpper, 50 ether, 50 ether);
+        uint128 liquidity1 = hook.addLiquidity(key, tickLower, tickUpper, amountToken0First, amountToken1First);
 
         // Verify no Aave deposits for in-range liquidity
         assertEq(
@@ -179,18 +193,21 @@ contract TestAddLiquidity is OntraHookFixture {
             "No token1 should be in Aave for in-range liquidity"
         );
 
+        uint256 amountToken0Second = 0;
+        uint256 amountToken1Second = 100 ether;
         // Now add more liquidity in a different range (above current tick)
         int24 tickLowerAbove = currentTick + 120;
         int24 tickUpperAbove = currentTick + 240;
 
-        uint128 liquidity2 = hook.addLiquidity(key, tickLowerAbove, tickUpperAbove, 0, 100 ether);
+        uint128 liquidity2 =
+            hook.addLiquidity(key, tickLowerAbove, tickUpperAbove, amountToken0Second, amountToken1Second);
 
         // Verify Aave deposits for out-of-range liquidity
         uint256 aaveToken0After = aavePool.getUserBalance(address(hook), address(token0));
         uint256 aaveToken1After = aavePool.getUserBalance(address(hook), address(token1));
 
         assertEq(aaveToken0After, aaveToken0Before, "No token0 should be deposited (above range)");
-        assertTrue(aaveToken1After > aaveToken1Before, "Token1 should be deposited to Aave");
+        assertEq(aaveToken1After - aaveToken1Before, 100 ether, "100 ether token1 should be deposited to Aave");
 
         // Verify liquidities
         assertGt(liquidity1, 0, "First liquidity should be positive (in-range)");
@@ -231,7 +248,7 @@ contract TestAddLiquidity is OntraHookFixture {
         uint256 aaveToken0After = aavePool.getUserBalance(address(hook), address(token0));
 
         // Verify large deposit
-        assertTrue(aaveToken0After > aaveToken0Before, "Large amount should be deposited");
+        assertEq(aaveToken0After - aaveToken0Before, 1000 ether, "1000 ether should be deposited");
         assertEq(liquidity, 0, "Liquidity should be 0 for Aave deposits");
     }
 
@@ -258,9 +275,9 @@ contract TestAddLiquidity is OntraHookFixture {
         uint256 aaveToken1After2 = aavePool.getUserBalance(address(hook), address(token1));
         uint256 secondDeposit = aaveToken1After2 - aaveToken1After1;
 
-        // Both deposits should be positive
-        assertGt(firstDeposit, 0, "First deposit should be positive");
-        assertGt(secondDeposit, 0, "Second deposit should be positive");
+        // Both deposits should be equal to 50 ether each
+        assertEq(firstDeposit, 50 ether, "First deposit should be 50 ether");
+        assertEq(secondDeposit, 50 ether, "Second deposit should be 50 ether");
         assertEq(liquidity1, 0, "First liquidity should be 0 for Aave deposits (out-of-range)");
         assertEq(liquidity2, 0, "Second liquidity should be 0 for Aave deposits (out-of-range)");
     }

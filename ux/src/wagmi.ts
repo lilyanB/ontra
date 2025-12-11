2e9f325973df3748bd93aaa6f1fd3c404fc4bcb1
lilyanb <lilyan.bastien@gmail.com>
2025-12-11 19:14:34 +0100

    feat: custom wallet

--- ux/src/wagmi.ts
+++ ux/src/wagmi.ts
@@ -1,14 +1,19 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { base, sepolia, baseSepolia } from "wagmi/chains";
+import { metaMaskWallet, rabbyWallet } from "@rainbow-me/rainbowkit/wallets";
+import { sepolia } from "wagmi/chains";
 
 export const config = getDefaultConfig({
   appName: "Ontra finance",
   projectId: "aaf7401b57e2657344cf0c05cddab898",
   chains: [
-    base,
-    baseSepolia,
     sepolia,
     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [sepolia] : []),
+  ],
+  wallets: [
+    {
+      groupName: "Recommended",
+      wallets: [metaMaskWallet, rabbyWallet],
+    },
   ],
   ssr: true,
 });
